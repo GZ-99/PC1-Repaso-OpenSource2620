fd62b905d2e515a62c3eebb9599cd074f4d9a319
GZ-99 <gabrielvilchezvite@gmail.com>
2026-09-24 17:14:37 -0500

    chore: update environments 2.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,8 @@
 export const environment = {
-  production: true,
-  medicalApiBaseUrl: 'https://www.nlm.nih.gov/mesh/meshhome.html',
-  medicalProviderSpecialtyEndpointPath: '/specialty',
+  production: false,
+  //medicalApiBaseUrl: 'https://www.nlm.nih.gov/mesh/meshhome.html'
+  medicalApiBaseUrl: '/specialties.json', //Esto es en caso de que la url no funcione
+  medicalProviderSpecialtyEndpointPath: '',
   logoProviderApiBaseUrl: 'https://img.logo.dev/',
   logoProviderPublishableKey: 'pk_H1j_uA9GRFizL2ikMw4qwQ'
 };
